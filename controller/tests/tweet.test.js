350723fbd3544a126ab200f223cc9a4f3e510afa
wjh <jh.won@kisos.net>
2022-06-08 19:35:12 +0900

    Tweet create testcode

--- controller/tests/tweet.test.js
+++ controller/tests/tweet.test.js
@@ -56,7 +56,7 @@ describe('TweetController', () => {
 			request = httpMocks.createRequest({
 				params: { id: tweetId },
 			});
-			response = httpMocks.createRequest();
+			response = httpMocks.createResponse();
 		});
 
 		it('returns the tweet if tweet exists', async () => {
@@ -66,24 +66,49 @@ describe('TweetController', () => {
 			await tweetController.getTweet(request, response);
 
 			expect(response.statusCode).toBe(200);
-			expect(response._getJSONData()).tpEqual(aTweet);
+			expect(response._getJSONData()).toEqual(aTweet);
 			expect(tweetRepository.getById).toHaveBeenCalledWith(tweetId);
 		})
 
 		it('returns 404 if tweet does not exist', async () => {
-			const aTweet = { text: faker.random.words(3) }
-			tweetRepository.getById = jest.fn(() => aTweet);
+			tweetRepository.getById = jest.fn(() => undefined);
 
 			await tweetController.getTweet(request, response);
 
-			expect(response.statusCode).toBe(200);
-			expect(response._getJSONData()).tpEqual(aTweet);
+			expect(response.statusCode).toBe(404);
+			expect(response._getJSONData()).toMatchObject({
+				message: `Tweet id(${tweetId}) not found`
+			});
 			expect(tweetRepository.getById).toHaveBeenCalledWith(tweetId);
 		})
 	})
 
 	describe('createTweet', () => {
-		it('returns all tweets when username is not provided', async () => {
+		let newTweet, authorId, request, response;
+		beforeEach(() => {
+			newTweet = faker.random.words(3);
+			authorId = faker.random.alphaNumeric(16);
+			request = httpMocks.createRequest({
+				body: { text: newTweet },
+				userId: authorId
+			})
+			response = httpMocks.createResponse();
+		})
+
+		it('returns 201 with created tweet object including userId', async () => {
+			tweetRepository.create = jest.fn((text, userId) => {
+				text,
+					userId
+			});
+
+			await tweetController.createTweet(request, response);
+
+			expect(response.statusCode).toBe(201);
+			expect(response._getJSONData()).toMatchObject({
+				text: newTweet,
+				userId: authorId,
+			});
+			expect(tweetRepository.create).toHaveBeenCalledWith(newTweet, authorId);
 		})
 	})
 
